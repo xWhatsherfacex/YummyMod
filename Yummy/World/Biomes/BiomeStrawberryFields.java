3716e2ec343c09cecce03203e71b92b4dc7964b5
abob78 <artin.alavi@gmail.com>
2016-02-25 16:59:10 -0800

    Update BiomeStrawberryFields.java

--- Yummy/World/Biomes/BiomeStrawberryFields.java
+++ Yummy/World/Biomes/BiomeStrawberryFields.java
@@ -6,17 +6,18 @@ import net.minecraft.init.Blocks;
 import net.minecraft.world.biome.BiomeGenBase;
 
 public class BiomeStrawberryFields extends BiomeGenBase{
-
-	private static final Height biomeHeight = new Height(0.1f,0.1f);
-	
 	public BiomeStrawberryFields(int id){
-	super(id);
-	this.setHeight(biomeHeight);
-	this.setColor(4699501);
-	this.setTemperatureRainfall(0.9F , 1.0F);
-	this.waterColorMultiplier = 3660528;
+		super(id);
+		this.setHeight(new Height(0.1f,0.1f));
+		this.setColor(4699501);
+		this.setTemperatureRainfall(0.9F , 1.0F);
+		this.spawnableMonsterList.clear();
+		this.spawnableCreatureList.clear();
+		//this.topBlock = ((byte)Main.MyGrass.blockID);
+		//this.fillerBlock = ((byte)Main.MyDirt.blockID);
+		this.setBiomeName("Stawberry Fields");
 
-	
+		this.waterColorMultiplier = 3660528;
 	}
 
 	@Override
