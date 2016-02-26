c99c49e2d3e9d04818a4dba54db86568a827e6d6
xWhatsherfacex <epicfailteen@gmail.com>
2016-02-25 19:38:16 -0600

    Added files via upload

---
+++ Yummy/YummyWorldGenerator.java
@@ -0,0 +1,18 @@
+package com.rnh.Yummy;
+
+import java.util.Random;
+
+import cpw.mods.fml.common.IWorldGenerator;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.IChunkProvider;
+
+public class YummyWorldGenerator implements IWorldGenerator{
+
+	@Override
+	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider chunkGenerator,
+			IChunkProvider chunkProvider) {
+
+		
+	}
+
+}
