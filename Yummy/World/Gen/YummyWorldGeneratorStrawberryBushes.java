b39edcb954da4832d8703c32450fc9488420ff14
xWhatsherfacex <epicfailteen@gmail.com>
2016-03-02 09:21:01 -0600

    Update YummyWorldGeneratorStrawberryBushes.java
    Fixed

--- Yummy/World/Gen/YummyWorldGeneratorStrawberryBushes.java
+++ Yummy/World/Gen/YummyWorldGeneratorStrawberryBushes.java
@@ -1,52 +1,62 @@
-package com.rnh.Yummy.World.Biomes;
+package com.rnh.Yummy.World.Gen;
 
 import java.util.Random;
 
 import com.rnh.Yummy.YummyBlocks;
-import com.rnh.Yummy.World.Gen.YummyWorldGeneratorStrawberryBushes;
 
 import cpw.mods.fml.common.IWorldGenerator;
-import net.minecraft.init.Blocks;
+import net.minecraft.block.Block;
 import net.minecraft.world.World;
-import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.chunk.IChunkProvider;
 import net.minecraft.world.gen.feature.WorldGenerator;
 
-public class BiomeStrawberryFields extends BiomeGenBase{
+public class YummyWorldGeneratorStrawberryBushes extends WorldGenerator{
+	 
+	
+	private Block StrawberryBush;
+    private int field_150551_b;
 
-	private static final Height biomeHeight = new Height(0.1f,0.1f);
-	
-	public BiomeStrawberryFields(int id){
-	super(id);
-	this.setBiomeName("StrawberryFields");
-	this.setHeight(biomeHeight);
-	this.setColor(4699501);
-	this.setTemperatureRainfall(0.9F , 1.0F);
-	this.waterColorMultiplier = 3660528;
-	
-	}
+    public YummyWorldGeneratorStrawberryBushes(Block block)
+    {
+        this.StrawberryBush = YummyBlocks.StrawberryBush;
+    }
 
-	@Override
-	public int getBiomeGrassColor( int x, int y, int z){
-		return 4699501;
-	}
-	@Override
-	public int getBiomeFoliageColor(int x, int y, int z){
-		return 7397529;
-	}
-	
-	@Override
-	public void decorate(World world, Random rand, int chunkX, int chunkZ)
-	{
-		super.decorate(world, rand, chunkX, chunkZ);
-		WorldGenerator strawberryBushGen = new YummyWorldGeneratorStrawberryBushes(topBlock);
-		final int MIN_PATCHES = 2; // change if needed
-		final int MAX_PATCHES = 10; // change if needed
-		for (int i = MIN_PATCHES + rand.nextInt(MAX_PATCHES - MIN_PATCHES); i > 0; i--) 
-		{
-	            int xRand = (chunkX * 16) + rand.nextInt(16) - rand.nextInt(16);
-	            int zRand = (chunkZ * 16) + rand.nextInt(16) - rand.nextInt(16);
-	            strawberryBushGen.generate(world, rand, xRand, 0, zRand);
-		}
-	}
-	
+    public void func_150550_a(Block block, int meta)
+    {
+        this.StrawberryBush = block;
+        this.field_150551_b = meta;
+    }
+    public int getAirBlockAboveGround(World world, int x, int z) 
+    {
+        int y = 255;
+        while(y > 0 && world.isAirBlock(x,y,z))
+        {
+            --y;
+        }
+        return y + 1;
+    }
+
+    @Override
+    public boolean generate(World world, Random random, int x, int y, int z)
+    {
+    	System.out.print("trying to generate bushes centered at " + x + ", " + z);
+    	
+    	final int RADIUS = 8;
+    	for (int l = 0; l < 64; ++l)
+    	{
+    		int tempX = x + random.nextInt(RADIUS) - random.nextInt(RADIUS);
+    		int tempZ = z + random.nextInt(RADIUS) - random.nextInt(RADIUS);
+    		int tempY = this.getAirBlockAboveGround(world, tempX, tempZ);
+    		System.out.print("Trying to place a bush at " + tempX + ", " + tempY + ", " + tempZ);
+
+    		if (world.isAirBlock(tempX, tempY, tempZ) && (!world.provider.hasNoSky || tempY < 255) && YummyBlocks.StrawberryBush.canBlockStay(world, tempX, tempY, tempZ))
+    		{
+    			world.setBlock(tempX, tempY, tempZ, YummyBlocks.StrawberryBush, 0, 3);
+    			System.out.print("Success!");
+    		}
+    	}
+
+    	return true;
+    }
+
 }
