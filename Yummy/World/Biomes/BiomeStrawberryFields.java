0e420573cf9671f845dc0d8f08d2317edfa24fed
xWhatsherfacex <epicfailteen@gmail.com>
2016-03-02 18:25:27 -0600

    Update BiomeStrawberryFields.java

--- Yummy/World/Biomes/BiomeStrawberryFields.java
+++ Yummy/World/Biomes/BiomeStrawberryFields.java
@@ -1,23 +1,28 @@
 package com.rnh.Yummy.World.Biomes;
 
-import com.rnh.Yummy.YummyBlocks;
+import java.util.Random;
 
+import com.rnh.Yummy.YummyBlocks;
+import com.rnh.Yummy.World.Gen.YummyWorldGeneratorStrawberryBushes;
+
+import cpw.mods.fml.common.IWorldGenerator;
 import net.minecraft.init.Blocks;
+import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
+import net.minecraft.world.gen.feature.WorldGenerator;
 
 public class BiomeStrawberryFields extends BiomeGenBase{
-	public BiomeStrawberryFields(int id){
-		super(id);
-		this.setHeight(new Height(0.1f,0.1f));
-		this.setColor(4699501);
-		this.setTemperatureRainfall(0.9F , 1.0F);
-		this.spawnableMonsterList.clear();
-		this.spawnableCreatureList.clear();
-		//this.topBlock = ((byte)Main.MyGrass.blockID);
-		//this.fillerBlock = ((byte)Main.MyDirt.blockID);
-		this.setBiomeName("Stawberry Fields");
 
-		this.waterColorMultiplier = 3660528;
+	private static final Height biomeHeight = new Height(0.1f,0.1f);
+	
+	public BiomeStrawberryFields(int id){
+	super(id);
+	this.setBiomeName("StrawberryFields");
+	this.setHeight(biomeHeight);
+	this.setColor(4699501);
+	this.setTemperatureRainfall(0.9F , 1.0F);
+	this.waterColorMultiplier = 3660528;
+	
 	}
 
 	@Override
@@ -28,7 +33,7 @@ public class BiomeStrawberryFields extends BiomeGenBase{
 	public int getBiomeFoliageColor(int x, int y, int z){
 		return 7397529;
 	}
-
+	
 	@Override
 	public void decorate(World world, Random rand, int chunkX, int chunkZ)
 	{
@@ -43,6 +48,5 @@ public class BiomeStrawberryFields extends BiomeGenBase{
 	            strawberryBushGen.generate(world, rand, xRand, 0, zRand);
 		}
 	}
-	
 	
 }
